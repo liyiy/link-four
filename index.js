3a8fa690f9797fd0ce2abdf3d408bd925911715b
liyi <liyiyu12@gmail.com>
2018-12-06 17:49:18 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+   const canvas = document.getElementById("game-canvas");
+   canvas.width = 700;
+   canvas.height = 500;
+
+   const context = canvas.getContext("2d");
+   
+});
